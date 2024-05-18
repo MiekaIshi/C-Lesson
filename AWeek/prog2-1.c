ae35d5c7482a4c1743da847ed36f429e6fd22604
mieka <miekachan.5656@gmail.com>
2024-05-19 01:03:14 +0900

    2章、3章

--- AWeek/prog2-1.c
+++ AWeek/prog2-1.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
- 
+
 void main(){
-    int a;
-    int b;                         //  変数a（キーボードからの値を代入)
-    printf("a=");                       //  キーボードからの入力を促す
-    scanf("%d",&a);
-    printf("\nb=" );                       //  キーボードからの入力を促す
-    scanf("%d",&b);                           //  キーボードから一文字入力
-    printf("a + b = %d" a + b);       //  aの値を表示
+	int a ;
+    int b ;
+	printf("a = ");
+	scanf("%d",&a);
+    printf("b = ");
+	scanf("%d",&b);
+    
+	printf("a + b = %d\n",a + b);
+    printf("a - b = %d\n",a - b);
+    printf("a * b = %d\n",a * b);
+    printf("a / b = %d\n",a / b);
+    
+    
+    
+
+    
+    
+
 }
+
--- AWeek/prog2-2.c
+++ AWeek/prog2-2.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
- 
+#include<stdio.h>
 void main(){
-    int a , b; 
-    printf("aを入力：") ;                            //  変数a（キーボードからの値を代入)              //  キーボードからの入力を促す
+    int a , b;
+    printf("�����`�̕��F");
     scanf("%d",&a);
-    printf("bを入力：") ;          
-    Scanf("%d",&b);                   //  キーボードから一文字入力
-    printf("a + b = %d ¥n",a+b);        //  aの値を表示
+    printf("�����`�̍���");
+    scanf("%d",&b);
+
+    printf("�����`�̖ʐς�%d",a * b);
 }
